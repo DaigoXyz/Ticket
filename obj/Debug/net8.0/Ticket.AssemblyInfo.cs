5d568da40abe865525d6922156189a13d9249fa4
DaigoX <dafaal171717@gmail.com>
2026-01-23 09:33:11 +0700

    undo localtime convert.

--- obj/Debug/net8.0/Ticket.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ticket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959c02dfb0eaf31fcb5c9a463daa473fa4acccb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
