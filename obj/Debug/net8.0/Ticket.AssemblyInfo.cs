802a0bab8a8a96672a6bbd7c6185e195e79698dc
DaigoX <dafaal171717@gmail.com>
2026-01-23 15:19:54 +0700

    fix waktu utc to local

--- obj/Debug/net8.0/Ticket.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ticket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d568da40abe865525d6922156189a13d9249fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035378845ac92be5f03c96742ebd0ad50831f755")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
